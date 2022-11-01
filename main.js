1557610999cd2d216054a21b2e130f25108fc626
Karan <rkrana811@gmail.com>
2022-11-01 00:07:47 -0500

    built the app
    created html, css, js files; added photos, icons. need to make the app responsive on mobile screens.

---
+++ main.js
@@ -0,0 +1,27 @@
+var btnTranslate = document.querySelector('#btn-translate');
+var textInput = document.querySelector('#txt-input'); 
+var outputDiv = document.querySelector('#output');
+var serverURL = "https://api.funtranslations.com/translate/sindarin.json"
+
+
+function errorHandler(error) {
+    console.log("An error occured");
+    alert("An error occured. Please try again after an hour.");
+}
+
+function getTranslationURL(test) {
+    return serverURL + "?" + "text=" + test
+}
+
+function clickEventHandler() {
+    fetch(getTranslationURL(textInput.value))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+        console.log(translatedText);
+    })
+    .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickEventHandler);
